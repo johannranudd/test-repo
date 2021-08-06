863b2740533cf6cb26d2eb97773ca41689283d20
johannranudd <33978097+johannranudd@users.noreply.github.com>
2021-08-06 17:37:52 +0200

    sdfg

--- js/script.js
+++ js/script.js
@@ -103,99 +103,99 @@
 
 // const ul = document.querySelector("ul");
 
-const ul1 = document.querySelector(".ul1");
-const ul2 = document.querySelector(".ul2");
-const ul3 = document.querySelector(".ul3");
-const li = document.querySelectorAll("li");
+// const ul1 = document.querySelector(".ul1");
+// const ul2 = document.querySelector(".ul2");
+// const ul3 = document.querySelector(".ul3");
+// const li = document.querySelectorAll("li");
 
-const companies = [
-  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
-  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
-  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
-  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
-  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
-  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
-  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
-  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
-  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
-];
+// const companies = [
+//   { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+//   { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+//   { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+//   { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+//   { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+//   { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+//   { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+//   { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+//   { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+// ];
 
-const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+// const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 // !test
-// createItem
-function createItem(item) {
-  const listItem = document.createElement("li");
-  listItem.classList.add("flexing");
-  listItem.innerHTML = `<span>${item.name}</span>
-    <span>${item.category}</span>
-    <span>${item.start}</span>
-    <span>${item.end}</span>`;
+// // createItem
+// function createItem(item) {
+//   const listItem = document.createElement("li");
+//   listItem.classList.add("flexing");
+//   listItem.innerHTML = `<span>${item.name}</span>
+//     <span>${item.category}</span>
+//     <span>${item.start}</span>
+//     <span>${item.end}</span>`;
 
-  return listItem;
-}
-// one has been returned
+//   return listItem;
+// }
+// // one has been returned
 
-// map and display items
-function displayItem(array, ulList) {
-  array.map((mapItem) => {
-    ulList.appendChild(createItem(mapItem));
-  });
-}
+// // map and display items
+// function displayItem(array, ulList) {
+//   array.map((mapItem) => {
+//     ulList.appendChild(createItem(mapItem));
+//   });
+// }
 
-// choose array to sort (in this case by start)
-function sortBy(arr) {
-  arr.sort((a, b) => {
-    if (a.start > b.start) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
-}
-// display items in the DOM
+// // choose array to sort (in this case by start)
+// function sortBy(arr) {
+//   arr.sort((a, b) => {
+//     if (a.start > b.start) {
+//       return 1;
+//     } else {
+//       return -1;
+//     }
+//   });
+// }
+// // display items in the DOM
 
-// sortBy(companies);
-// displayItem(companies, ul1)
+// // sortBy(companies);
+// // displayItem(companies, ul1)
 
-// filter items
-function filterByStartAndCategory(array, val) {
-  const filterArr = array.filter((item) => {
-    return item.start >= val || item.category === val;
-  });
-  return filterArr;
-}
+// // filter items
+// function filterByStartAndCategory(array, val) {
+//   const filterArr = array.filter((item) => {
+//     return item.start >= val || item.category === val;
+//   });
+//   return filterArr;
+// }
 
-// displayItem(filterByStartAndCategory(companies, 'Retail'), ul2)
+// // displayItem(filterByStartAndCategory(companies, 'Retail'), ul2)
 
-function filterByMoreThanTenYears(arr, val) {
-  const filterArr = arr.filter((item) => {
-    return item.end - item.start >= val;
-  });
-  return filterArr;
-}
-// filterByMoreThanTenYears(companies, 10)
-// displayItem(filterByMoreThanTenYears(companies, 1999), ul3)
-// displayItem(filterByMoreThanTenYears(companies, 10), ul3)
+// function filterByMoreThanTenYears(arr, val) {
+//   const filterArr = arr.filter((item) => {
+//     return item.end - item.start >= val;
+//   });
+//   return filterArr;
+// }
+// // filterByMoreThanTenYears(companies, 10)
+// // displayItem(filterByMoreThanTenYears(companies, 1999), ul3)
+// // displayItem(filterByMoreThanTenYears(companies, 10), ul3)
 
-const buttons = document.querySelectorAll("#btn");
-let editFlag = false;
+// const buttons = document.querySelectorAll("#btn");
+// let editFlag = false;
 
-buttons.forEach(function(btn) {
-    btn.addEventListener("click", function(e) {
-        const id = e.target.dataset.id;
-        ul1.innerHTML = '';
+// buttons.forEach(function(btn) {
+//     btn.addEventListener("click", function(e) {
+//         const id = e.target.dataset.id;
+//         ul1.innerHTML = '';
         
-        if (id === "10-years-pluss") {
-          displayItem(filterByMoreThanTenYears(companies, 10), ul1);
-        } else if (id === "Retail") {
-          displayItem(filterByStartAndCategory(companies, "Retail"), ul1);
-        } 
+//         if (id === "10-years-pluss") {
+//           displayItem(filterByMoreThanTenYears(companies, 10), ul1);
+//         } else if (id === "Retail") {
+//           displayItem(filterByStartAndCategory(companies, "Retail"), ul1);
+//         } 
         
-    })
+//     })
     
     
-})
+// })
  
 
 // sortBy(companies);
@@ -258,3 +258,45 @@ buttons.forEach(function(btn) {
 // }
 
 // mapFunction(filterCompanies);
+
+
+// !new test
+
+const ul1 = document.querySelector(".ul1");
+const buttons = document.querySelector('.button-div');
+
+const companies = [
+  { name: "Company One", category: "Finance", start: 1981, end: 2004 },
+  { name: "Company Two", category: "Retail", start: 1992, end: 2008 },
+  { name: "Company Three", category: "Auto", start: 1999, end: 2007 },
+  { name: "Company Four", category: "Retail", start: 1989, end: 2010 },
+  { name: "Company Five", category: "Technology", start: 2009, end: 2014 },
+  { name: "Company Six", category: "Finance", start: 1987, end: 2010 },
+  { name: "Company Seven", category: "Auto", start: 1986, end: 1996 },
+  { name: "Company Eight", category: "Technology", start: 2011, end: 2016 },
+  { name: "Company Nine", category: "Retail", start: 1981, end: 1989 },
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+buttons.addEventListener("click", function(e) {
+  const id = e.target.dataset.id;
+  if (id === 'Retail') {
+    ul1.innerHTML = displayItem(companies);
+  }
+})
+
+function createItem(item) {
+  const listItem = 
+}
+
+
+function displayItem(array) {
+  array.map(mapItem => {
+    return `<span>${mapItem.name}</span>
+    <span>${mapItem.category}</span>
+    <span>${mapItem.start}</span>
+    <span>${mapItem.end}</span>`;
+  })
+  return array;
+}
